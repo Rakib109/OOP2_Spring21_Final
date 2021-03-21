6d751230a958ad5d2764d754985391c7ab2c2362
rakib <19-39358-1@student.aiub.edu>
2021-03-21 23:56:04 +0600

    FLab 1

---
+++ 21.03 Task/21.03 Task/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank_Account
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Account ss = new SuperSavings("salmir", "004", 1000f);
+            
+            ss.ShowInfo();
+
+            Account overdraft = new Overdraft("kaif", "005", 1000f, 5000);
+            overdraft.Transfer(ss, 1000);
+            overdraft.ShowInfo();
+
+           
+
+        }
+    }
+}
---
+++ 21.03 Task/21.03 Task/Transaction.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank_Account
+{
+    class Transaction
+    {
+        private Account sender;
+        private Account receiver;
+        public double Amount { get; set; }
+        public string Additional { get; set; }
+
+
+
+        public void showTransactions()
+        {
+            Console.WriteLine("Transaction type: " + Additional);
+            if (Additional.ToLower().Equals("transfer"))
+            {
+                Console.WriteLine("Sender: " + sender);
+                Console.WriteLine("Receiver: " + receiver);
+            }
+            Console.WriteLine("Amount: " + Amount);
+        }
+
+    }
+}
